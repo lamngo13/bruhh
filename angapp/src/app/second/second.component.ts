b1f51d9de3ac8cd65c955c3e634360b8aa93c8dd
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2024-07-31 17:43:46 -0400

    added gallery

--- angapp/src/app/second/second.component.ts
+++ angapp/src/app/second/second.component.ts
@@ -7,12 +7,31 @@ import { Router } from '@angular/router';
   styleUrl: './second.component.css'
 })
 export class SecondComponent {
+
+  images = [
+    'assets/img/image0.jpeg',
+    'assets/img/image1.jpeg',
+    'assets/img/image3.jpeg',
+    'assets/img/image4.jpeg',
+    'assets/img/image5.jpeg',
+    'assets/img/image6.jpeg',
+    'assets/img/image7.jpeg',
+  ]
+  currentImage = 0;
   
     constructor(private router: Router) {}
   
     goHome() {
       console.log('home');
       this.router.navigate(['/home']);
+    }
+
+    prevImage() {
+      this.currentImage = (this.currentImage - 1 + this.images.length) % this.images.length;
+    }
+  
+    nextImage() {
+      this.currentImage = (this.currentImage + 1) % this.images.length;
     }
 
 }
