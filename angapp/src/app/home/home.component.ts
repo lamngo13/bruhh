31562c08afd3f206338924446f3364e698cbaf42
lamngo13 <69515114+lamngo13@users.noreply.github.com>
2024-10-31 16:51:59 -0400

    heart follows cursor

--- angapp/src/app/home/home.component.ts
+++ angapp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ElementRef } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -11,21 +12,52 @@ export class HomeComponent {
 
   language: string = 'en';
   gifPosition = {x: 0, y: 0};
-  min_image_distance = 40;
-  smoothing_value = 0.2;
+  min_image_distance = 10;
+  smoothing_value = 0.04;
+  cursorPosition = { x: 0, y: 0 };
+  intervalId: any;
 
   constructor(private router: Router,
               private elementRef: ElementRef
   ) {}
 
-  onMouseMove(event: MouseEvent) {
-    const cursorX = event.clientX + window.scrollX;
-    const cursorY = event.clientY + window.scrollY;
+  ngOnInit() {
+    // Start the interval to update the GIF position
+    this.intervalId = setInterval(() => this.updateGifPosition(), 16); // ~60 FPS
+  }
 
+  ngOnDestroy() {
+    // Clear the interval when the component is destroyed
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.cursorPosition.x = event.clientX + window.scrollX;
+    this.cursorPosition.y = event.clientY + window.scrollY;
+    // const cursorX = event.clientX + window.scrollX;
+    // const cursorY = event.clientY + window.scrollY;
+
+    // // Calculate the distance between the cursor and the GIF's current position
+    // const distanceX = cursorX - this.gifPosition.x;
+    // const distanceY = cursorY - this.gifPosition.y;
+    // const distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+
+    // // Move only if beyond the threshold distance
+    // if (distance > this.min_image_distance) {
+    //   // Update the position incrementally towards the cursor
+    //   this.gifPosition.x += distanceX * this.smoothing_value;
+    //   this.gifPosition.y += distanceY * this.smoothing_value;
+    // }
+  }
+
+  updateGifPosition() {
     // Calculate the distance between the cursor and the GIF's current position
-    const distanceX = cursorX - this.gifPosition.x;
-    const distanceY = cursorY - this.gifPosition.y;
-    const distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+    const distanceX = this.cursorPosition.x - this.gifPosition.x;
+    const distanceY = this.cursorPosition.y - this.gifPosition.y;
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
 
     // Move only if beyond the threshold distance
     if (distance > this.min_image_distance) {
